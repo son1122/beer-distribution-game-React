125d154d775634ee5986c7c318527af53479daf4
son1122 <wongsatorn.n@hotmail.com>
2022-12-01 11:59:33 +0700

    Update UI

--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import "./Main.css";
+import { useNavigate } from "react-router-dom";
 
 const Main = (props) => {
+  const navigate = useNavigate();
   const [infoPage, setInfoPage] = useState(1);
   let maxPage = 4;
   let list = props.countryCode.map((loop, index) => {
@@ -29,31 +31,100 @@ const Main = (props) => {
           >
             &#8249;
           </p>
-          <h1 style={{ textAlign: "center" }}>This is the homepage!</h1>
-          <p onClick={() => {
+          <h1 style={{ textAlign: "center" }}>
+            &emsp;&emsp;INFORMATION&emsp;&emsp;
+          </h1>
+          <p
+            onClick={() => {
               setInfoPage(infoPage >= maxPage ? 1 : infoPage + 1);
               console.log(infoPage);
             }}
-            className="next round">&#8250;</p>
-        </div>
-        <h3 style={{ marginLeft: "6%", marginRight: "10%" }}>
-          &emsp;&emsp;&emsp;&emsp;&emsp; Beer Distribution Game is Financial
-          Simulation Game that simulate product distribution across 4 player
-          that act as Retailer Wholesale Distributor Manufactoring (Player 1 to
-          4 in order) to simulate and show the problem in Distribution System.
-          This Website aim to improve Beer Game with more function and plannig
-          for user to gain more fun with adaptive Simulation game you can find
-          source code{" "}
-          <a
-            target="_blank"
-            href="https://github.com/son1122/beer-distribution-game-React"
+            className="next round"
           >
-            HERE
-          </a>
-          If you have any Suggestion or anything Please let me know.Thank you
-          every one for your attention
-        </h3>
-        <img src="https://zensimu.com/assets/static/beergame-header-visual.06976bc.0a1ff2bb7cc7457c115dd1b15a568c0c.png" />
+            &#8250;
+          </p>
+        </div>
+        {infoPage == 1 && (
+          <div>
+            <h2 style={{ textAlign: "center" }}>BEER Distribution Game</h2>
+            <h3 style={{ marginLeft: "6%", marginRight: "10%" }}>
+              &emsp;&emsp;&emsp;&emsp;&emsp; Beer Distribution Game is Financial
+              Simulation Game that simulate product distribution across 4 player
+              to simulate and show the problem in Distribution System. This
+              Website aim to improve Beer Game with more function user to gain
+              more fun with adaptive Simulation game you can find source code{" "}
+              <a
+                target="_blank"
+                href="https://github.com/son1122/beer-distribution-game-React"
+              >
+                HERE 
+              </a>
+              If you have any Suggestion or anything Please let me know.Thank
+              you every one for your attention
+            </h3>
+            <img src="https://zensimu.com/assets/static/beergame-header-visual.06976bc.0a1ff2bb7cc7457c115dd1b15a568c0c.png" />
+          </div>
+        )}
+        {infoPage == 2 && (
+          <div>
+            <h2 style={{ textAlign: "center" }}>Feature List</h2>
+            <ul style={{marginLeft : "10%"}}>
+            <li style={{fontSize : "1.2em"}}>Playable Beer Distribution Game</li>
+              <li style={{fontSize : "1.2em"}}>Sale Number That Reatrailer receive is affect by Country Event and Holiday</li>
+              <li style={{fontSize : "1.2em"}}>Sale Number That Reatrailer receive True random</li>
+              <li style={{fontSize : "1.2em"}}>48 Turn Game = year and 1 Turn = 1 weak of the month</li>
+              <li style={{fontSize : "1.2em"}}>4 Player Local Game</li>
+              <li style={{fontSize : "1.2em"}}>Each Player COST of operation</li>
+              <li style={{fontSize : "1.2em"}}>Chart , data and Summary of Each Player</li>
+
+            </ul>
+            <h2 style={{ textAlign: "center" }}>Functional List </h2>
+            <ul style={{marginLeft : "10%"}}>
+              <li style={{fontSize : "1.2em"}}>true random number generator (TRNG) from <a
+                target="_blank"
+                href="https://www.random.org/"
+              >
+                APIs
+              </a></li>
+              <li style={{fontSize : "1.2em"}}>React Chart from Google<a
+                target="_blank"
+                href="https://www.react-google-charts.com/"
+              >
+                Chart
+              </a></li>
+              <li style={{fontSize : "1.2em"}}>Holiday DATA by api-ninjas.com<a
+                target="_blank"
+                href="https://api-ninjas.com/api/holidays"
+              >
+                APIs
+              </a></li>
+              <li style={{fontSize : "1.2em"}}>Country DATA by restcountries.com<a
+                target="_blank"
+                href="https://restcountries.com/v3.1/alpha?codes=th"
+              >
+                APIs
+              </a></li>
+          
+            </ul>
+            <p style={{ textAlign: "center" ,marginLeft : "10%"}}>**Thank you every developer who contribute and provide all data and API**</p>
+          </div>
+        )}
+        {infoPage == 3 && (
+          <div>
+            <h2 style={{ textAlign: "center" }}>How to Play a Game</h2>
+            <h3 style={{ marginLeft: "6%", marginRight: "10%" }}>
+              &emsp;&emsp;&emsp;&emsp;&emsp; Step
+            </h3>
+          </div>
+        )}
+        {infoPage == 4 && (
+          <div>
+            <h2 style={{ textAlign: "center" }}>Curios about RESULT ?? ME 2</h2>
+            <h3 style={{ marginLeft: "6%", marginRight: "10%" }}>
+              &emsp;&emsp;&emsp;&emsp;&emsp; Result
+            </h3>
+          </div>
+        )}
       </div>
       <div className="country">
         <h2>Country List For you to Select</h2>
@@ -75,6 +146,16 @@ const Main = (props) => {
             style={{ width: "30%", height: "25%" }}
           >
             Press this to clear
+          </button>
+        )}
+        {props.country != null && (
+          <button
+            onClick={() => {
+              return navigate("/game")
+            }}
+            style={{ width: "30%", height: "25%" }}
+          >
+            Start !!!!!
           </button>
         )}
       </div>
